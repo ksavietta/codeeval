5076973d31f73bba9e9f3ad018454696b773cd08
Kelly Savietta <ksavietta@gmail.com>
2014-01-31 15:59:52 -0500

    added sum of integers and happy number exercises, happy number is not submitted yet

---
+++ easy/happy-numbers/happy_numbers.rb
@@ -0,0 +1,33 @@
+# CHALLENGE DESCRIPTION:
+
+# A happy number is defined by the following process. Starting with any positive integer, replace the number by the sum of the squares of its digits,
+# and repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
+# Those numbers for which this process ends in 1 are happy numbers, while those that do not end in 1 are unhappy numbers.
+
+# For the curious, here's why 7 is a happy number: 7->49->97->130->10->1.
+# Here's why 22 is NOT a happy number: 22->8->64->52->29->85->89->145->42->20->4->16->37->58->89 ...
+
+require 'pry'
+
+class HappyNumber
+  def self.transform_happy_number(line)
+    happy_number_array = line.split("").map {|x| x.to_i}
+    total = 0
+    squared_array = happy_number_array.map {|x| x**2}
+    total = squared_array.inject{|sum,x| sum+x}
+    number_path_array = []
+    if total == 1
+      puts "hooray"
+    elsif number_path_array.include?(total)
+      puts "nope"
+    else
+      number_path_array << total
+      line = total.to_s
+      HappyNumber.transform_happy_number(line)
+    end
+  end
+end
+
+File.open(ARGV[0]).each_line do |line|
+  HappyNumber.transform_happy_number(line)
+end
