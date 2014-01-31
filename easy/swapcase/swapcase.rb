cb08e2ba61c709df788a384a1622106bbb33cca4
Kelly Savietta <ksavietta@gmail.com>
2014-01-31 13:39:38 -0500

    swapcase submitted to codeeval

--- easy/swapcase/swapcase.rb
+++ easy/swapcase/swapcase.rb
@@ -1,8 +1,14 @@
-def swap_case(sentence)
-  x = sentence.split.map! {|word| word.swapcase}
-  x.join(" ")
-end
+# def swap_case(sentence)
+#   x = sentence.split.map! {|word| word.swapcase}
+#   x.join(" ")
+# end
+
+# File.open(ARGV[0]).each_line do |line|
+#   puts swap_case(line)
+# end
+
 
 File.open(ARGV[0]).each_line do |line|
-  puts swap_case(line)
+  x = line.split.map! {|word| word.swapcase}
+  puts x.join(" ")
 end
